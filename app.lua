c6f99f6534c71117ca495d9c0f6299b2f0e2e9ac
Alexey Gladtsinov <alexey.gladtsinov@media-tel.ru>
2018-05-17 17:40:54 +0300

    print session object

--- app.lua
+++ app.lua
@@ -1,8 +1,16 @@
 local lapis = require("lapis")
 local app = lapis.Application()
 
+
+app:before_filter(function(self)
+    print("session tables")
+    for  key, value in pairs(self.session) do
+        print(key .. value)
+    end
+end)
+
 app:get("/", function()
-  return "Welcome to Lapis " .. require("lapis.version")
+    return "Welcome to Lapis " .. require("lapis.version")
 end)
 
 return app
