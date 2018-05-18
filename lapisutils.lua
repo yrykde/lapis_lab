c5d54c7081043318264a81c680365264c0a586e2
Alexey Gladtsinov <alexey.gladtsinov@media-tel.ru>
2018-05-18 12:11:42 +0300

    insert closure

--- lapisutils.lua
+++ lapisutils.lua
@@ -1,21 +1,24 @@
 local lapisutils = {}
 
 function lapisutils.table_represent(table, deph)
-    if deph == nil then
-        deph = 1
-    end
-    if table == nil then
-        io.write("NIL valie")
-        return
-    end
-    for key, value in pairs(table) do
-        if type(value) == "table" then
-            io.write(string.format(string.rep("\t", deph).."[%s] => table\n", tostring(key)))
-            table_represent(table[key], deph+1)
-        else
-            io.write(string.format(string.rep("\t", deph).."[%s] => [%s]\n", tostring(key), tostring(value)))
+    function table_represent_inside()
+        if deph == nil then
+            deph = 1
+        end
+        if table == nil then
+            io.write("NIL valie")
+            return
+        end
+        for key, value in pairs(table) do
+            if type(value) == "table" then
+                io.write(string.format(string.rep("\t", deph).."[%s] => table\n", tostring(key)))
+                table_represent_inside(table[key], deph+1)
+            else
+                io.write(string.format(string.rep("\t", deph).."[%s] => [%s]\n", tostring(key), tostring(value)))
+            end
         end
     end
+    table_represent_inside(table, deph)
 end
 
 return lapisutils
